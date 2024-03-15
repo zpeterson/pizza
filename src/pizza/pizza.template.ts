2e7cdc170f65cb79aa46794ed2373fefd639edcd
Zoë Peterson <zoepeterson@microsoft.com>
2024-03-15 10:43:05 -0500

    Add option to pause animation

--- src/pizza/pizza.template.ts
+++ src/pizza/pizza.template.ts
@@ -1,11 +1,17 @@
 import { html, ref } from "@microsoft/fast-element";
+import { classNames } from '@microsoft/fast-web-utilities';
+
 import { Pizza } from "./pizza";
 
 export const template = html<Pizza>`
 <div class="pizza-container">
     <div class="circle">
         <div class="logo"></div>
-        <div class="text" ${ref('text')}></div>
+        <div
+            ${ref('text')}
+            class=${x => classNames('text', ['animate', x.animated])}
+            @click=${x => x.animationClick()}
+        ></div>
     </div>
 </div
 `;
