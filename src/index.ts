d3fa873a0e1f19d74369297dd03cae909f34c18e
Zoë Peterson <zoepeterson@microsoft.com>
2024-03-18 15:07:36 -0500

    Add prettier and eslint (#6)

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,22 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { app } from './pizza/pizza';
 import { settings } from './settings/settings';
 
-
-provideFluentDesignSystem().register(
-    fluentSelect(),
-    fluentOption()
-);
+provideFluentDesignSystem().register(fluentSelect(), fluentOption());
 
 i18next
-.use(HttpBackend)
-.use(LanguageDetector)
-.init({
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .init({
     backend: {
-        loadPath: '/locales/{{lng}}.json'
+      loadPath: '/locales/{{lng}}.json'
     },
     fallbackLng: 'en',
-    debug: true,
-}).then(() => {
+    debug: true
+  })
+  .then(() => {
     // Define the elements once i18next is initialized
     app.define();
     settings.define();
-});
+  });
 
 export default i18next;
--- src/pizza/pizza.template.ts
+++ src/pizza/pizza.template.ts
@@ -1,19 +1,19 @@
-import { html, ref } from "@microsoft/fast-element";
+import { html, ref } from '@microsoft/fast-element';
 import { classNames } from '@microsoft/fast-web-utilities';
 
-import { Pizza } from "./pizza.js";
-import  "../settings/settings.js";
+import { Pizza } from './pizza.js';
+import '../settings/settings.js';
 
 export const template = html<Pizza>`
-<pizza-settings></pizza-settings>
-<div class="pizza-container">
+  <pizza-settings></pizza-settings>
+  <div class="pizza-container">
     <div class="circle">
-        <div class="logo"></div>
-        <div
-            ${ref('text')}
-            class=${x => classNames('text', ['animate', x.animated])}
-            @click=${x => x.animationClick()}
-        ></div>
+      <div class="logo"></div>
+      <div
+        ${ref('text')}
+        class=${x => classNames('text', ['animate', x.animated])}
+        @click=${x => x.animationClick()}
+      ></div>
     </div>
-</div>
+  </div>
 `;
