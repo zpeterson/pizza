a6c87aec746244361da69f835fa1ac74b5928b84
Zoë Peterson <zoepeterson@microsoft.com>
2024-03-21 11:41:48 -0500

    try pizza/

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pizza',
+  base: '/pizza/',
   build: {
     lib: {
       entry: 'src/index.ts',
